972cef579cb65e9ebbbc2bedbbab651b0973dce9
Jason Keenan <jason.keenan@raavin.com>
2016-03-20 14:32:27 +1100

    finished hashes 6

--- hashes/exercise_six.rb
+++ hashes/exercise_six.rb
@@ -1,13 +1,10 @@
-=beging
+=begin
 I started with this strategy but got a bit stuck so have looked at the strategy suggested by launchschool.
 Sorting is definitely the easiest way to go but in the interest of not cheating, I have decided to continue on to see if mu original strategy would work.
 Currently I can identify the anagrams and, cheating a bit with uniq can get them in order.
 anagrams_all seems to capture the first iteration but am still trying to figure out how I can start a new group of words in a new array.
 currently the first array keeps getting added to.
 =end
-words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
-          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
-          'flow', 'neon', 'flfub', 'flufm']
 
 =begin
 get first word
@@ -23,26 +20,33 @@ push word to new array if
 #p words[0][0]
 #p words[0].split("").include?(words[target_word_index][letter])
 
-anagrams_all = []
-anagrams = []
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon', 'flfub', 'flufm']
 
-words.each_with_index do |item, reference_word_index|
-	words.each_with_index do |item, index|
-		#checks that length is same then checks if all letters are there
-		if words[reference_word_index].length == words[index].length &&
-			 words[reference_word_index].split("").include?(words[index][0]) && 
-			 words[reference_word_index].split("").include?(words[index][1]) &&
-			 words[reference_word_index].split("").include?(words[index][2]) &&
-		   words[reference_word_index].split("").include?(words[index][3])
+def sort_anagrams(words)
+	anagrams_all = []
 
-			 #puts item + " is anagram & index is " + index.to_s
-			 anagrams << item
-			 #p anagrams
-		#else 
-		#	 puts "not anagram"
+	words.each_with_index do |item, reference_word_index|
+
+		anagrams = []
+
+		words.each_with_index do |item, index|
+			#checks that length is same then checks if all letters are there
+			if words[reference_word_index].length == words[index].length &&
+				 words[reference_word_index].split("").include?(words[index][0]) && 
+				 words[reference_word_index].split("").include?(words[index][1]) &&
+				 words[reference_word_index].split("").include?(words[index][2]) &&
+			   words[reference_word_index].split("").include?(words[index][3])
+
+				 anagrams << item
+			end
 		end
+		anagrams_all << anagrams.uniq
 	end
-	anagrams_all << anagrams.uniq
+	anagrams_all.uniq
 end
-#p anagrams.uniq
-p anagrams_all
+
+sort_anagrams(words).each_with_index do |array,index|
+	p sort_anagrams(words)[index]
+end
